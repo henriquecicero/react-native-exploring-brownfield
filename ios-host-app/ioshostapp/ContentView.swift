361bf3e58431357bdb9fc823beebced1dca93a62
Henrique Cícero <henriquecicero@gmail.com>
2026-01-17 22:04:53 -0300

    chore: ios host app initial boilerplate

---
+++ ios-host-app/ioshostapp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ioshostapp
+//
+//  Created by Henrique Cícero on 17/01/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, iOS!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios-host-app/ioshostapp/ioshostappApp.swift
@@ -0,0 +1,17 @@
+//
+//  ioshostappApp.swift
+//  ioshostapp
+//
+//  Created by Henrique Cícero on 17/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct ioshostappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
