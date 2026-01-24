7d20230a00393d658b20bef86daad44cc8d78aa1
Henrique Cícero <henriquecicero@gmail.com>
2026-01-23 21:58:02 -0300

    feat: enable back navigation in react native activity

--- android-host-app/app/src/main/java/com/example/androidhostapp/ReactNativeActivity.kt
+++ android-host-app/app/src/main/java/com/example/androidhostapp/ReactNativeActivity.kt
@@ -1,5 +1,7 @@
 package com.example.androidhostapp
 
+import android.os.Bundle
+import android.view.MenuItem
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -7,8 +9,22 @@ import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class ReactNativeActivity : ReactActivity() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
     override fun getMainComponentName(): String = "reactnativeembeddedapp"
 
     override fun createReactActivityDelegate(): ReactActivityDelegate =
         DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return if (item.itemId == android.R.id.home) {
+            finish()
+            true
+        } else {
+            super.onOptionsItemSelected(item)
+        }
+    }
 }
